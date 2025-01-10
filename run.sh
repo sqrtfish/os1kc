a5ce8de0722d0e8ee6383d44dede5d97fd10c433
sqrtfish <bbg0yz@gmail.com>
2025-01-10 15:20:04 +0800

    Hello World!

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ CC=/usr/local/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # new: build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
