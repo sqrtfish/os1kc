dfd87a7759c6f356a864accb2da5e1421bae1ec0
sqrtfish <bbg0yz@gmail.com>
2025-02-07 11:06:06 +0800

    disk io

--- run.sh
+++ run.sh
@@ -35,4 +35,6 @@ $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw,if=none \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
     -kernel kernel.elf # new: load the kernel
